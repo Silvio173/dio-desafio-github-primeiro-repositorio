133263707201d8829ac7c83bfc42722e008b2dd5
Silvio173 <silvio.sgmtec@gmail.com>
2021-08-04 19:46:40 -0300

    Primeiro Commit

---
+++ src/br/com/sgmtec/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.sgmtec;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Silvio", "Negro", 47);
+        int a = 2;
+        int b = 5;
+        System.out.println("Helo Word" + (a + b));
+        System.out.println(gato);
+    }
+}
